039be22eee289991efb092095c4a0e64c97401c8
Jan Graichen <jg@altimos.de>
2013-03-20 21:26:56 +0100

    Add basic resource module module.

---
+++ lib/acfs/collection.rb
@@ -0,0 +1,12 @@
+module Acfs
+
+  # A wrapper for collection of resources providing
+  # methods to access singular or multiple resources.
+  #
+  class Collection
+
+    def initialize(name, opts = {})
+
+    end
+  end
+end
--- lib/acfs/resources.rb
+++ lib/acfs/resources.rb
@@ -1,3 +1,5 @@
+require 'acfs/collection'
+
 module Acfs
 
   # Allows to define and load API resources in a client class.
@@ -6,12 +8,23 @@ module Acfs
   #     resources :users
   #   end
   #
-  #   MyClient.new.fetch do |client|
-  #     @user = client.users.fetch(1) do |user|
-  #       @comments = user.comments.all
-  #     end
+  #   client = MyClient.new
+  #   @user = client.users.fetch(1) do |user|
+  #     @comments = user.comments.all
   #   end
   #
+  # Returned objects are proxies that will contain no data until
+  # request are fired and responses processed:
+  #
+  #    client.run
+  #
+  # Resource calls can also be automatically processed by wrapping
+  # all in `Acfs::Client.run`:
+  #
+  #    client.run do |cl|
+  #      @user = cl.users.fetch(1)
+  #    end
+  #    @user.name # => "Anon"
   #
   module Resources
     extend ActiveSupport::Concern
@@ -24,8 +37,15 @@ module Acfs
       #     resources :users
       #   end
       #
+      # Will create a getter returning a resource collection
+      # for accessing finders and loading sets of resources.
+      #
       def resources(name, opts = {})
+        collection = Collection.new name, opts
 
+        define_method name.to_sym do
+          collection
+        end
       end
     end
   end
