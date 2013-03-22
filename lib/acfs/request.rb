4aeda01bf32abb14aeb4f3772a46a39425c3ad95
Jan Graichen <jg@altimos.de>
2013-03-22 22:25:12 +0100

    Just strip query from Request url. Params option should be used instead.

--- lib/acfs/request.rb
+++ lib/acfs/request.rb
@@ -1,3 +1,5 @@
+require 'cgi'
+
 require 'acfs/request/callbacks'
 
 module Acfs
@@ -9,11 +11,14 @@ module Acfs
     include Request::Callbacks
 
     def initialize(url, options = {})
-      @url     = url
-      @headers = options.delete(:headers) || {}
-      @params  = options.delete(:params) || {}
-      @data    = options.delete(:data) || nil
-      @format  = options.delete(:format) || :json
+      @url = URI.parse(url).tap do |url|
+        @data    = options.delete(:data) || nil
+        @format  = options.delete(:format) || :json
+        @headers = options.delete(:headers) || {}
+        @params  = options.delete(:params) || {}
+
+        url.query = params.any? ? params.to_param : nil
+      end.to_s
     end
 
     def data?
--- spec/acfs/request_spec.rb
+++ spec/acfs/request_spec.rb
@@ -4,13 +4,30 @@ describe Acfs::Request do
   let(:url)     { 'http://api.example.org/v1/examples' }
   let(:headers) { nil }
   let(:params)  { nil }
-  let(:body)    { nil }
-  let(:options) { {headers: headers, params: params, body: body} }
+  let(:data)    { nil }
+  let(:options) { {headers: headers, params: params, data: data} }
   let(:request) { Acfs::Request.new(url, options) }
 
   describe '#url' do
     it 'should return request URL' do
       expect(request.url).to be == url
+    end
+
+    context 'with parameters' do
+      let(:params) { { id: 10 }}
+
+      it 'should return URL with query' do
+        expect(request.url).to be == "#{url}?id=10"
+      end
+    end
+
+    context 'with parameters in URL' do
+      let(:url) { 'http://api.example.org/v1/examples?b=ac' }
+      let(:params) { { id: 10 }}
+
+      it 'should strip query from URL and append params' do
+        expect(request.url).to be == 'http://api.example.org/v1/examples?id=10'
+      end
     end
   end
 
@@ -19,6 +36,14 @@ describe Acfs::Request do
 
     it 'should return request headers' do
       expect(request.headers).to be == headers
+    end
+  end
+
+  describe '#params' do
+    let(:params) { { id: 10 }}
+
+    it 'should return request headers' do
+      expect(request.params).to be == params
     end
   end
 end
