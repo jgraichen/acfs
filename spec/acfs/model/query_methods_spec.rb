9f95fea101025fa50ac92ab25340a960651acd33
Jan Graichen <jg@altimos.de>
2013-05-01 15:30:46 +0200

    Add more specs for fetching resources on error cases.

--- spec/acfs/model/query_methods_spec.rb
+++ spec/acfs/model/query_methods_spec.rb
@@ -5,28 +5,30 @@ describe Acfs::Model::QueryMethods do
 
   describe '.find' do
     context 'with single id' do
-      before do
-        stub_request(:get, 'http://users.example.org/users/1').to_return(
-            body: MessagePack.dump({ id: 1, name: 'Anon', age: 12 }),
-            headers: {'Content-Type' => 'application/x-msgpack'})
-      end
-
-      it 'should load a single remote resource' do
-        user = model.find 1
-        Acfs.run
-
-        expect(user.attributes).to be == { id: 1, name: 'Anon', age: 12 }.stringify_keys
-      end
-
-      it 'should invoke callback after model is loaded' do
-        proc = Proc.new { }
-        proc.should_receive(:call) do |user|
-          expect(user).to be === @user
-          expect(user).to be_loaded
+      context 'with successful response' do
+        before do
+          stub_request(:get, 'http://users.example.org/users/1').to_return(
+              body: MessagePack.dump({ id: 1, name: 'Anon', age: 12 }),
+              headers: {'Content-Type' => 'application/x-msgpack'})
         end
 
-        @user = model.find 1, &proc
-        Acfs.run
+        it 'should load a single remote resource' do
+          user = model.find 1
+          Acfs.run
+
+          expect(user.attributes).to be == { id: 1, name: 'Anon', age: 12 }.stringify_keys
+        end
+
+        it 'should invoke callback after model is loaded' do
+          proc = Proc.new { }
+          proc.should_receive(:call) do |user|
+            expect(user).to be === @user
+            expect(user).to be_loaded
+          end
+
+          @user = model.find 1, &proc
+          Acfs.run
+        end
       end
 
       context 'with 404 response' do
@@ -45,6 +47,22 @@ describe Acfs::Model::QueryMethods do
           expect(@user).to_not be_loaded
         end
       end
+
+      context 'with 500 response' do
+        before do
+          stub_request(:get, 'http://users.example.org/users/1').to_return(
+              status: 500,
+              headers: {'Content-Type' => 'text/plain'})
+        end
+
+        it 'should raise a response error' do
+          @user = model.find 1
+
+          expect { Acfs.run }.to raise_error(Acfs::ErroneousResponse)
+
+          expect(@user).to_not be_loaded
+        end
+      end
     end
 
     context 'with multiple ids' do
@@ -57,25 +75,42 @@ describe Acfs::Model::QueryMethods do
             headers: {'Content-Type' => 'application/x-msgpack'})
       end
 
-      it 'should load a multiple remote resources' do
-        users = model.find 1, 2
-        Acfs.run
+      context 'with successful response' do
+        it 'should load a multiple remote resources' do
+          users = model.find 1, 2
+          Acfs.run
 
-        expect(users.size).to be == 2
-        expect(users[0].attributes).to be == { id: 1, name: 'Anon', age: 12 }.stringify_keys
-        expect(users[1].attributes).to be == { id: 2, name: 'Johnny', age: 42 }.stringify_keys
-      end
-
-      it 'should invoke callback after all models are loaded' do
-        proc = Proc.new { }
-        proc.should_receive(:call) do |users|
-          expect(users).to be === @users
           expect(users.size).to be == 2
-          expect(users).to be_loaded
+          expect(users[0].attributes).to be == { id: 1, name: 'Anon', age: 12 }.stringify_keys
+          expect(users[1].attributes).to be == { id: 2, name: 'Johnny', age: 42 }.stringify_keys
         end
 
-        @users = model.find 1, 2, &proc
-        Acfs.run
+        it 'should invoke callback after all models are loaded' do
+          proc = Proc.new { }
+          proc.should_receive(:call) do |users|
+            expect(users).to be === @users
+            expect(users.size).to be == 2
+            expect(users).to be_loaded
+          end
+
+          @users = model.find 1, 2, &proc
+          Acfs.run
+        end
+      end
+
+      context 'with one 404 response' do
+        before do
+          stub_request(:get, 'http://users.example.org/users/1').to_return(
+              status: 404,
+              body: MessagePack.dump({ error: 'not found' }),
+              headers: {'Content-Type' => 'application/x-msgpack'})
+        end
+
+        it 'should raise resource not found error' do
+          model.find 1, 2
+
+          expect { Acfs.run }.to raise_error(Acfs::ResourceNotFound)
+        end
       end
     end
   end
