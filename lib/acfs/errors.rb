2ab94dc8d70f201f87a94666f94c39f14848a67e
Jan Graichen <jg@altimos.de>
2013-05-01 15:23:41 +0200

    Add error classes for not found resources and failed responses.

--- lib/acfs/errors.rb
+++ lib/acfs/errors.rb
@@ -2,6 +2,22 @@ module Acfs
 
   # Acfs base error.
   #
-  class Error < StandardError; end
+  class Error < StandardError
+  end
+
+  # Response error containing the responsible response object.
+  #
+  class ErroneousResponse < Error
+    attr_accessor :response
+
+    def initialize(response)
+      self.response = response
+    end
+  end
+
+  # Resource not found error raised on a 404 response
+  #
+  class ResourceNotFound < ErroneousResponse
+  end
 
 end
