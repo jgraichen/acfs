eff9044bca7ba6e09083e74e62c9f13264cb4d70
Jan Graichen <jg@altimos.de>
2013-05-07 20:28:18 +0200

    Add specs for Service::Middleware.use.

---
+++ spec/acfs/service/middleware_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+class TestMiddleware < Acfs::Middleware::Base
+end
+
+describe Acfs::Service::Middleware do
+  let(:srv_class) { Class.new(Acfs::Service) }
+  let(:options) { {} }
+  let(:service) { srv_class.new options }
+  let(:middleware) { TestMiddleware }
+
+  describe '.use' do
+    let(:options) { { abc: 'cde' } }
+
+    it 'should add middleware to list' do
+      srv_class.use middleware
+
+      expect(srv_class.instance_variable_get(:@middlewares)).to include(middleware)
+    end
+
+    it 'should add middleware to stack' do
+      srv_class.use middleware
+
+      expect(srv_class.middleware).to be_a(middleware)
+    end
+
+    it 'should instantiate middleware object' do
+      middleware.should_receive(:new).with(anything, options)
+
+      srv_class.use middleware, options
+    end
+  end
+end
