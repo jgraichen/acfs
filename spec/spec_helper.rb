da096c8cbcafb76ce93670ba4d0792c6637f646e
Jan Graichen <jg@altimos.de>
2013-05-25 10:09:49 +0200

    Require acfs after coverage in spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
-require 'acfs'
 require 'coveralls'
 Coveralls.wear! do
   add_filter 'spec'
 end
 
+require 'acfs'
 require 'webmock/rspec'
 
 Dir[File.expand_path('spec/support/**/*.rb')].each {|f| require f}
