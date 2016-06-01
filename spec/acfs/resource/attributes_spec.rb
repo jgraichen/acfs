jgraichen/acfs

commit f7e410976f07172fa8fdb0a45d6bcb25a42f5ab5

Change attribute handling by storing arbitrary attribute type objects.

lib/acfs/model/attributes.rb
@@ -125,11 +125,11 @@ module Acfs::Model
     # @raise [ ArgumentError ] If no attribute with given name is defined.
     #
     def write_attribute(name, value, opts = {})
-      if (type = self.class.attribute_types[name.to_sym]).nil?
+      if (attr = self.class.defined_attributes[name.to_s]).nil?
         raise ArgumentError.new "Unknown attribute `#{name}`."
       end
 
-      write_raw_attribute name, value.nil? ? nil : type.cast(value), opts
+      write_raw_attribute name, attr.cast(value), opts
     end
 
     # @api private
@@ -187,7 +187,15 @@ module Acfs::Model
       # @return [ Hash{ String => Object, Proc } ] Attributes with default values.
       #
       def attributes
-        @attributes ||= {}.merge superclass.respond_to?(:attributes) ? superclass.attributes : {}
+        Hash.new.tap do |attrs|
+          defined_attributes.each do |key, attr|
+            attrs[key] = attr.default_value
+          end
+        end
+      end
+
+      def defined_attributes
+        @attributes ||= {}.merge superclass.respond_to?(:defined_attributes) ? superclass.defined_attributes : {}
       end
 
       # @api public
@@ -211,12 +219,9 @@ module Acfs::Model
       private
       def define_attribute(name, type, opts = {})
         name             = name.to_s
-        type             = type.new opts
-        default_value    = opts.has_key?(:default) ? opts[:default] : nil
-        default_value    = type.cast default_value unless default_value.is_a? Proc or default_value.nil?
+        attribute        = type.new opts
 
-        attributes[name] = default_value
-        attribute_types[name.to_sym] = type
+        defined_attributes[name] = attribute
         define_attribute_method name
 
         self.send :define_method, name do

spec/acfs/model/attributes_spec.rb
@@ -115,39 +115,41 @@ describe Acfs::Model::Attributes do
     end
   end
 
-  describe '.attribute' do
-    it 'should add an attribute to model attribute list' do
-      model.send :attribute, :name, :string
+  describe 'class' do
+    describe '#attribute' do
+      it 'should add an attribute to model attribute list' do
+        model.send :attribute, :name, :string
 
-      expect(model.attributes).to be == { :name => nil }.stringify_keys
-    end
+        expect(model.attributes).to be == { :name => nil }.stringify_keys
+      end
 
-    it 'should accept a default value' do
-      model.send :attribute, :name, :string, default: 'John'
+      it 'should accept a default value' do
+        model.send :attribute, :name, :string, default: 'John'
 
-      expect(model.attributes).to be == { :name => 'John' }.stringify_keys
-    end
+        expect(model.attributes).to be == { :name => 'John' }.stringify_keys
+      end
 
-    it 'should accept an symbolic type' do
-      model.send :attribute, :age, :integer, default: '12'
+      it 'should accept an symbolic type' do
+        model.send :attribute, :age, :integer, default: '12'
 
-      expect(model.attributes).to be == { :age => 12 }.stringify_keys
-    end
+        expect(model.attributes).to be == { :age => 12 }.stringify_keys
+      end
 
-    it 'should accept an class type' do
-      model.send :attribute, :age, Acfs::Model::Attributes::Integer, default: '12'
+      it 'should accept an class type' do
+        model.send :attribute, :age, Acfs::Model::Attributes::Integer, default: '12'
 
-      expect(model.attributes).to be == { :age => 12 }.stringify_keys
-    end
+        expect(model.attributes).to be == { :age => 12 }.stringify_keys
+      end
 
-    context 'allow nil option' do
-      it 'should allow nil as value' do
-        model.send :attribute, :updated_at, Acfs::Model::Attributes::DateTime, default: DateTime.new, allow_nil: true
-        resource = model.new
-        expect(resource.updated_at).to eq DateTime.new
+      context 'allow nil option' do
+        it 'should allow nil as value' do
+          model.send :attribute, :updated_at, Acfs::Model::Attributes::DateTime, default: DateTime.new, allow_nil: true
+          resource = model.new
+          expect(resource.updated_at).to eq DateTime.new
 
-        resource.updated_at = ''
-        expect(resource.updated_at).to eq nil
+          resource.updated_at = ''
+          expect(resource.updated_at).to eq nil
+        end
       end
     end
   end

lib/acfs/model/attributes/base.rb
@@ -5,15 +5,24 @@ module Acfs::Model::Attributes
 
     def initialize(opts = {})
       @options = opts
+      @options.reverse_merge! allow_nil: true
     end
 
-    def allow_nil?
+    def nil_allowed?
       !!options[:allow_nil]
     end
-    alias_method :nil_allowed?, :allow_nil?
 
     def default_value
-      options[:default]
+      options[:default].is_a?(Proc) ? options[:default] : cast(options[:default])
+    end
+
+    def cast(obj)
+      return nil if obj.nil? && nil_allowed?
+      cast_type obj
+    end
+
+    def cast_type(obj)
+      raise NotImplementedError
     end
   end
 end

lib/acfs/model/attributes/boolean.rb
@@ -27,7 +27,7 @@ module Acfs::Model
       # @param [Object] obj Object to cast.
       # @return [TrueClass, FalseClass] Casted boolean.
       #
-      def cast(obj)
+      def cast_type(obj)
         return true if obj.is_a? TrueClass
         return false if obj.is_a? FalseClass
 

lib/acfs/model/attributes/date_time.rb
@@ -20,7 +20,7 @@ module Acfs::Model::Attributes
     # @param [Object] obj Object to cast.
     # @return [DateTime] Casted object as DateTime.
     #
-    def cast(obj)
+    def cast_type(obj)
       return nil if nil_allowed? and obj.blank?
       return obj if obj.is_a? ::DateTime
       return ::DateTime.iso8601(obj.iso8601) if obj.is_a? Time or obj.is_a? Date

lib/acfs/model/attributes/float.rb
@@ -19,7 +19,7 @@ module Acfs::Model::Attributes
     # @param [Object] obj Object to cast.
     # @return [Float] Casted object as float.
     #
-    def cast(obj)
+    def cast_type(obj)
       obj.to_f
     end
   end

lib/acfs/model/attributes/integer.rb
@@ -19,7 +19,7 @@ module Acfs::Model::Attributes
     # @param [Object] obj Object to cast.
     # @return [Fixnum] Casted object as fixnum.
     #
-    def cast(obj)
+    def cast_type(obj)
       obj.to_i
     end
   end

lib/acfs/model/attributes/list.rb
@@ -20,7 +20,7 @@ module Acfs::Model::Attributes
     # @return [Fixnum] Casted object as list.
     # @raise [TypeError] If object cannot be casted to a list.
     #
-    def cast(obj)
+    def cast_type(obj)
       return obj.to_a if obj.respond_to? :to_a
       raise TypeError.new "Cannot cast #{obj.inspect} to array."
     end

commit a274c952fdae86f12d7a3d4fadbb785ca51c7c55

Fix attribute definitions on inherited resources
Inherited resources did not correctly inherited defined attributes from
the superclass since commit 7cf1d1135 (Apr 2014, v0.43). This commit
fixes the lookup of defined attributes and adds some specs for testing the
lookup. It also removes the #attribute_types method broken since commit
f7e4109 (Sep 2013, v0.23).

lib/acfs/resource/attributes.rb
@@ -222,56 +222,30 @@ class Acfs::Resource
       #   Attributes with default values.
       #
       def attributes
-        {}.tap do |attrs|
-          defined_attributes.each do |key, attr|
-            attrs[key] = attr.default_value
-          end
+        defined_attributes.each_with_object({}) do |(key, attr), hash|
+          hash[key] = attr.default_value
         end
       end
 
       def defined_attributes
-        @attributes ||= begin
-          attributes = {}
-          if superclass.respond_to?(:defined_attributes)
-            attributes.merge superclass.defined_attributes
-          end
-
-          attributes
-        end
-      end
-
-      # @api public
-      #
-      # Return hash of attributes and there types.
-      #
-      # @example
-      #   class User < Acfs::Resource
-      #     attribute :name, :string
-      #     attribute :age, :integer, default: 25
-      #   end
-      #   User.attributes # => {"name": Acfs::Model::Attributes::String,
-      #                   #     "age":  Acfs::Model::Attributes::Integer}
-      #
-      # @return [Hash{Symbol => Class}] Attributes and their types.
-      #
-      def attribute_types
-        @attribute_types ||= begin
-          attribute_types = {}
-          if superclass.respond_to?(:attribute_types)
-            attribute_types.merge superclass.attribute_types
-          end
-
-          attribute_types
+        if superclass.respond_to?(:defined_attributes)
+          superclass.defined_attributes.merge(local_attributes)
+        else
+          local_attributes
         end
       end
 
       private
 
+      def local_attributes
+        @local_attributes ||= {}
+      end
+
       def define_attribute(name, type, opts = {})
         name      = name.to_s
         attribute = type.new opts
 
-        defined_attributes[name] = attribute
+        local_attributes[name] = attribute
         define_attribute_method name
 
         send :define_method, name do

spec/acfs/resource/attributes_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Acfs::Resource::Attributes do
   let(:model) { Class.new Acfs::Resource }
+  let(:submodel) { Class.new model }
 
   describe '#initialize' do
     before { model.attribute :name, :string, default: 'John' }
@@ -137,7 +138,7 @@ describe Acfs::Resource::Attributes do
   end
 
   describe 'class' do
-    describe '#attribute' do
+    describe '#attributes' do
       it 'should add an attribute to model attribute list' do
         model.send :attribute, :name, :string
 
@@ -161,6 +162,17 @@ describe Acfs::Resource::Attributes do
           default: '12'
 
         expect(model.attributes.symbolize_keys).to eq age: 12
+      end
+
+      context 'on inherited resources' do
+        before do
+          model.attribute :age, :integer, default: 5
+          submodel.attribute :born_at, :date_time
+        end
+
+        it 'includes superclass attributes' do
+          expect(submodel.attributes.keys).to match_array %w(age born_at)
+        end
       end
     end
   end