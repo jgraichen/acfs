f3d1c4a5088088bf92f2a0b586963802cb581b82
Jan Graichen <jg@altimos.de>
2016-08-05 14:04:57 +0200

    Rebuild middleware stack without ActionDispatch
    Since 0.44 the middleware stack was build using
    ActionDispatch::MiddlewareStack. Unfortunatly this had some problems
    when using NewRelic with Rails as NewRelic RPM patches
    ActionDispatch::MiddlewareStack for tracing. It only expects Rack ENVs -
    with Acfs::Requests beeing incompatible - leading to NoMethodErrors.
    
    This commit rebuilds the middleware stack class without using
    ActionDispatch only providing the minimal required interface.

--- lib/acfs/service/middleware/stack.rb
+++ lib/acfs/service/middleware/stack.rb
@@ -1,28 +1,57 @@
-require 'action_dispatch/middleware/stack'
-
 module Acfs
   class Service
     module Middleware
-      class Stack < ActionDispatch::MiddlewareStack
+      class Stack
+        include Enumerable
+
         MUTEX = Mutex.new
         IDENTITY = -> (i) { i }
 
-        def build!
-          MUTEX.synchronize do
-            return if @stack
+        attr_reader :middlewares
 
-            @stack = build
+        def initialize
+          @middlewares = []
+        end
+
+        def call(request)
+          build! unless @app
+
+          @app.call request
+        end
+
+        def build!
+          return if @app
+
+          MUTEX.synchronize do
+            return if @app
+
+            @app = build
           end
         end
 
         def build(app = IDENTITY)
-          super
+          middlewares.reverse.inject(app) do |next_middleware, current_middleware|
+            klass, args, block = current_middleware
+            args ||= []
+
+            if klass.is_a?(Class)
+              klass.new(next_middleware, *args, &block)
+            elsif klass.respond_to?(:call)
+              lambda do |env|
+                next_middleware.call(klass.call(env, *args))
+              end
+            else
+              fail "Invalid middleware, doesn't respond to `call`: #{klass.inspect}"
+            end
+          end
         end
 
-        def call(request)
-          build! unless @stack
+        def insert(index, klass, *args, &block)
+          middlewares.insert(index, [klass, args, block])
+        end
 
-          @stack.call request
+        def each
+          middlewares.each { |x| yield x.first }
         end
 
         def clear
